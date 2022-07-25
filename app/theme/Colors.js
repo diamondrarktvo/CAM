d35930f166bf94fb1097b6f1ad7e6a4805ddeb66
amada10 <diamondrap20.aps1b@gmail.com>
2022-07-25 22:46:47 +0300

    Insert dans Colors

--- app/theme/Colors.js
+++ app/theme/Colors.js
@@ -2,6 +2,6 @@ export const Colors = {
    purple: '#7360e3', //purple
    grey: '#B8CBD0', //grey
    white: '#FAFAFA', //white,
-   black: '#0C0F0A',
+   black: '#0C0F0A', //noir
    background: '#F3F2FF', //background
 };
