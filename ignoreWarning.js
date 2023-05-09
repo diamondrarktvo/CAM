ac26bc08204d7143a961a4eeca2167635c70446a
diamondrarktvo <amada.rktvo@gmail.com>
2023-05-09 22:55:05 +0300

    feat: updating expo sdk
    bug: migrate from carousel to carousel
    to flatlist or flashlist

--- ignoreWarning.js
+++ ignoreWarning.js
@@ -5,7 +5,7 @@ if (__DEV__) {
       // "EventEmitter.removeListener",
       // "[fuego-swr-keys-from-collection-path]",
       // "Setting a timer for a long period of time",
-      'ViewPropTypes will be removed from React Native',
+      //'ViewPropTypes will be removed from React Native',
       // "AsyncStorage has been extracted from react-native",
       // "exported from 'deprecated-react-native-prop-types'.",
       // "Non-serializable values were found in the navigation state.",
